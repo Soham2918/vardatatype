71f9442c1815aa9751ccddf6cffadf4277a1affc
Soham2918 <joshipg1@gmail.com>
2022-11-07 13:52:35 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let mobile=99999988888
+console.log(mobile)
+let firstName='pras'
+let secondName='josh'
+let address=`Mumbai ${firstName} ${secondName}`
+console.log(firstName)
+console.log(secondName)
+console.log(address)
+
+let student={
+            studenName:'fixed',
+            emailId:'student@gamil.com',
+            phone:84154121545,
+            location:`Pune`
+}
+
+console.log(student.location)
+console.log(student)
